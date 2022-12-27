e16ac039a3d4db65fa73a190673db58f6f5e09e9
Nikeshbhalerao123 <nikeshbhalerao1998@gmail.com>
2022-12-27 07:30:03 +0530

    First Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.dictionaryangad11dec {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.dictionaryangad11dec to javafx.fxml;
+    exports com.example.dictionaryangad11dec;
+}
